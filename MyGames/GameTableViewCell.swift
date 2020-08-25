2b685d53a218712da0ddf4614797ce1f20bbb5c8
Tamires Pereira <tamirespereira6@gmail.com>
2020-08-24 21:52:34 -0300

    layout

---
+++ MyGames/GameTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GameTableViewCell.swift
+//  MyGames
+//
+//  Created by tamires.p.silva on 24/08/20.
+//  Copyright © 2020 Tamires. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var ivCover: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbConsole: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyGames/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  MyGames
+//
+//  Created by tamires.p.silva on 24/08/20.
+//  Copyright © 2020 Tamires. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
